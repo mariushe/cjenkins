e0ad144d21e379c9a7bf22394d86337a435e8c06
Marius Herring <mariusherring@gmail.com>
2013-12-20 21:26:08 +0100

    Changed structure

--- cjenkins.py
+++ cjenkins.py
@@ -3,6 +3,7 @@
 import curses
 import urllib
 import sys
+import time
 
 def createHeader():
 
@@ -21,25 +22,36 @@ def init():
 	x,y = myscreen.getmaxyx();
 	curses.start_color()
 	curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_RED)
-	createHeader()
-	readData()
+	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
-	myscreen.refresh()
-	myscreen.getch()
-	curses.endwin()
+
+def displayGui():
+	while 1:
+		createHeader()
+		readData()
+		myscreen.refresh()
+
+		time.sleep(1)
+
+
 
 def readData():
 
 	row = 2
-	data = eval(urllib.urlopen("").read());
+	data = eval(urllib.urlopen("http://jenkins.sendregning.no:8090/api/python?pretty=true").read());
 
 	for current in data["jobs"]:
 
 		nameToDisplay = current["name"].strip();
-		myscreen.addstr(row, 2, nameToDisplay)
+		myscreen.addstr(row, 2, nameToDisplay, curses.color_pair(2))
+		myscreen.addstr(row, 30, "||||||||", curses.color_pair(3))
 		row += 1
 
 
 init();
+displayGui()
+	
+curses.endwin()
 
 readData();
